6ca2489723e3c228499a6db02d8eb6ba3bf86093
Egor Maslov <102520104+Henge96@users.noreply.github.com>
2023-04-18 18:21:32 +0300

    api in service - add valdiator + validation list request (#16)
    * feat: init kitchen
    
    * feat: add app and queue layers in service + repo
    
    * feat: part of app process and repo layer in service
    
    * feat: add app/process/api layers in service
    
    * feat: add validator in api struct + validate list cooking method
    
    ---------
    
    Co-authored-by: EgorMaslov <egor.maslov1696@gmail.com>

--- cmd/kitchen/internal/api/http/http.go
+++ cmd/kitchen/internal/api/http/http.go
@@ -2,24 +2,35 @@ package http
 
 import (
 	"context"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofrs/uuid/v5"
 	"github.com/gorilla/mux"
 	"net/http"
 	"xsolla/cmd/kitchen/internal/app"
 )
 
-type application interface {
-	ListCooking(ctx context.Context, params app.CookingParams) ([]app.Cooking, int, error)
-	ChangeCookingStatus(ctx context.Context, cookingID uuid.UUID, status app.CookingStatus) error
-}
+type (
+	application interface {
+		ListCooking(ctx context.Context, params app.CookingParams) ([]app.Cooking, int, error)
+		ChangeCookingStatus(ctx context.Context, cookingID uuid.UUID, status app.CookingStatus) error
+	}
+
+	requestValidator interface {
+		Struct(s interface{}) error
+	}
+)
 
 type api struct {
-	app application
+	app       application
+	validator requestValidator
 }
 
 func New(app application) http.Handler {
+	v := validator.New()
+
 	a := &api{
 		app,
+		v,
 	}
 
 	r := mux.NewRouter()
--- cmd/kitchen/internal/api/http/model.go
+++ cmd/kitchen/internal/api/http/model.go
@@ -1,6 +1,15 @@
 package http
 
-import "xsolla/cmd/kitchen/internal/app"
+import (
+	"fmt"
+	"xsolla/cmd/kitchen/internal/app"
+)
+
+type requestListOfCooking struct {
+	CookingStatus app.CookingStatus `json:"cooking_status" validate:"required"`
+	Limit         uint16            `json:"limit" validate:"gt=0"`
+	Offset        uint16            `json:"offset"`
+}
 
 func toAppCookingStatus(in string) (app.CookingStatus, error) {
 	switch in {
@@ -13,6 +22,6 @@ func toAppCookingStatus(in string) (app.CookingStatus, error) {
 	case app.CookingStatusCompleted.String():
 		return app.CookingStatusCompleted, nil
 	default:
-		return 0, app.ErrInvalidArgument
+		return 0, fmt.Errorf("unknown cooking status: %s: %w", in, app.ErrInvalidArgument)
 	}
 }
